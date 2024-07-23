65d1a0a0fb1add0b7cf09608cf17ce132bec1ad9
Ayush <Balbeershingrawat@gmail.com>
2024-07-23 12:01:12 +0530

    recurrsive sorting Algorithms

---
+++ Basic-Questions/MergeSort.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void Mg(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int l = low;
+    int r = mid + 1;
+    while (l <= mid && r <= high)
+    {
+        if (arr[l] <= arr[r])
+        {
+            temp.push_back(arr[l]);
+            l++;
+        }
+        else
+        {
+            temp.push_back(arr[r]);
+            r++;
+        }
+    }
+      while (l <= mid)
+        {
+            temp.push_back(arr[l]);
+            l++;
+        }
+        while (r <= high)
+        {
+            temp.push_back(arr[r]);
+            r++;
+        }
+        for (int i = low; i <= high; i++)
+        {
+            arr[i] = temp[i - low];
+        }
+}
+void MgSt(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    MgSt(arr, low, mid);
+    MgSt(arr, mid + 1, high);
+    Mg(arr, low, mid, high);
+}
+
+int main()
+{
+    vector<int> ans = {3, 1, 6, 2, 9, 7, 12};
+    MgSt(ans, 0, ans.size() - 1);
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+    return 0;
+}
---
+++ Basic-Questions/QuickSort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int piviot(vector<int>&arr , int low , int high){
+    int piviot = arr[low];
+    int h = high;
+    int l = low;
+    l++;
+    while(l <= h){
+        while(arr[l] < piviot){
+            l++;
+        }
+        while(arr[h] > piviot){
+            h--;
+        }
+        if(l<=h){swap(arr[l] , arr[h]);}
+    }
+    swap(arr[low], arr[h]);
+    return h;
+}
+
+void quickSort(vector<int>&arr,int low, int high){
+    if(low<high){
+        int pIdx =  piviot(arr, low , high);
+        quickSort(arr, low , pIdx-1);
+        quickSort(arr, pIdx+1 , high);
+    }
+}
+
+int main() {
+     vector<int> ans = {0, 1, 4, 2, 9, 7, 12};
+    quickSort(ans, 0, ans.size()-1);
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+    return 0;
+}
